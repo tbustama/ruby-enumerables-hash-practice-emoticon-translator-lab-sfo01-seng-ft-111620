9720b2f2bcfb396e5f88b7c6a40ecd7b60913f6b
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-04 18:17:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,7 @@
 # require modules here
 require "yaml"
 require "pry"
+
 def load_library(file)
   new_hash = {}
   new_file = YAML.load_file(file)
@@ -18,13 +19,10 @@ def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  hash = load_library(file)
+  hash.each do |k1, v1|
+    
+  end
 end
 
-  # {
-  #     'happy' => {
-  #       :english => ":)",
-  #       :japanese => "(＾ｖ＾)"
-  #     }
-  # }
