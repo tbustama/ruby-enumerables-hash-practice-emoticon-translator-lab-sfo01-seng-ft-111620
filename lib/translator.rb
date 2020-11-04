3449321565e95110eb332923af08c42f8413c1ee
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-04 18:12:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,16 @@
 require "yaml"
 require "pry"
 def load_library(file)
+  new_hash = {}
   new_file = YAML.load_file(file)
+  new_file.each do |k1, v1|
+    new_hash[k1] = {
+      english: v1[0],
+      japanese: v1[0]
+    }
+  end
   binding.pry
-  
+  new_hash
 end
 
 def get_japanese_emoticon
